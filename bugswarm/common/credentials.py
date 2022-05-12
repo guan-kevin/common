994bff3b7e20ded883c42531dc36d1607969c1e6
Kevin Guan <github@pigzy.net>
2022-05-12 11:00:08 -0700

    Update common

--- bugswarm/common/credentials.py
+++ bugswarm/common/credentials.py
@@ -1,3 +1,4 @@
 # DockerHub
 DOCKER_HUB_USERNAME = 'bugswarm'
 DOCKER_HUB_REPO = '{}/{}'.format(DOCKER_HUB_USERNAME, 'images')
+DOCKER_HUB_CACHED_REPO = '{}/{}'.format(DOCKER_HUB_USERNAME, 'cached-images')
